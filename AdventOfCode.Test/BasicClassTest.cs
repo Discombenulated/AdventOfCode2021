c1346224e1ec314848b3fd89a5f4fbb19c9f64cf
Discombenulated <ben [at] benshouse.net>
2021-11-30 17:08:15 +0000

    [BN] Mostly checking my git config

--- AdventOfCode.Test/BasicClassTest.cs
+++ AdventOfCode.Test/BasicClassTest.cs
@@ -15,4 +15,11 @@ public class BasicClassTest
         var bc = new BasicClass();
         Assert.IsTrue(bc.TestsWork());
     }
+
+    [Test]
+    public void Test2()
+    {
+        var bc = new BasicClass();
+        Assert.AreEqual(true, bc.TestsWork());
+    }
 }
